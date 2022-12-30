4bbca0d5131a8a9c3da4e77992f147bb07c9ded2
Luc <luc@lucemans.nl>
2022-12-30 19:07:17 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = Express();
 
 connect(process.env.DB_CONNECT!, () => {
     console.log("Connected to db");
-})
+});
 
 app.use((req, res, next) => {
     console.log(`${req.method} on ${req.path} from ${req.ip}`);
@@ -19,11 +19,12 @@ app.use(json());
 
 app.get("/", (req, res) => {
     res.send({ status: 200 });
-})
+});
 
 app.use("/api/auth/", AuthHandler);
 
 const _PORT = process.env.PORT || 8080;
+
 app.listen(_PORT, () => {
     console.log("Listening on", _PORT);
-})
+});
